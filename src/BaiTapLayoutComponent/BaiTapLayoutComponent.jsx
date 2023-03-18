27009d3b3c57933622c6233688ecf2261cb5092d
lokilamduc <119769138+lokilamduc@users.noreply.github.com>
2023-03-18 18:45:32 +0700

    update file my-app

---
+++ src/BaiTapLayoutComponent/BaiTapLayoutComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Body from './Body'
+import Footer from './Footer'
+import Header from './Header'
+
+
+function BaiTapLayoutComponent() {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    )
+}
+
+export default BaiTapLayoutComponent
---
+++ src/BaiTapLayoutComponent/Item.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+function Item() {
+  return (
+    <section className="pt-4">
+  <div className="container px-lg-5">
+    {/* Page Features*/}
+    <div className="row gx-lg-5">
+      <div className="col-lg-6 col-xxl-4 mb-5">
+        <div className="card bg-light border-0 h-100">
+          <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4 h-25 justify-content-center d-inline-flex p-3 position-absolute top-0 start-50 translate-middle align-items-center"><h3><i className="bi bi-collection" /></h3></div>
+            <h2 className="fs-4 fw-bold mt-4 pt-5">Fresh new layout</h2>
+            <p className="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6 col-xxl-4 mb-5">
+        <div className="card bg-light border-0 h-100">
+          <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4 h-25 justify-content-center d-inline-flex p-3 position-absolute top-0 start-50 translate-middle align-items-center"><h3><i className="bi bi-cloud-download" /></h3></div>
+            <h2 className="fs-4 fw-bold mt-4 pt-5">Free to download</h2>
+            <p className="mb-0">As always, Start Bootstrap has a powerful collectin of free templates.</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6 col-xxl-4 mb-5">
+        <div className="card bg-light border-0 h-100">
+          <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4 h-25 justify-content-center d-inline-flex p-3 position-absolute top-0 start-50 translate-middle align-items-center"><h3><i className="bi bi-card-heading" /></h3></div>
+            <h2 className="fs-4 fw-bold mt-4 pt-5">Jumbotron hero header</h2>
+            <p className="mb-0">The heroic part of this template is the jumbotron hero header!</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6 col-xxl-4 mb-5">
+        <div className="card bg-light border-0 h-100">
+          <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4 h-25 justify-content-center d-inline-flex p-3 position-absolute top-0 start-50 translate-middle align-items-center"><h3><i className="bi bi-bootstrap" /></h3></div>
+            <h2 className="fs-4 fw-bold mt-4 pt-5">Feature boxes</h2>
+            <p className="mb-0">We've created some custom feature boxes using Bootstrap icons!</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6 col-xxl-4 mb-5">
+        <div className="card bg-light border-0 h-100">
+          <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4 h-25 justify-content-center d-inline-flex p-3 position-absolute top-0 start-50 translate-middle align-items-center"><h3><i className="bi bi-code" /></h3></div>
+            <h2 className="fs-4 fw-bold mt-4 pt-5">Simple clean code</h2>
+            <p className="mb-0">We keep our dependencies up to date and squash bugs as they come!</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6 col-xxl-4 mb-5">
+        <div className="card bg-light border-0 h-100">
+          <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4 h-25 justify-content-center d-inline-flex p-3 position-absolute top-0 start-50 translate-middle align-items-center"><h3><i className="bi bi-patch-check" /></h3></div>
+            <h2 className="fs-4 fw-bold mt-4 pt-5">A name you trust</h2>
+            <p className="mb-0">Start Bootstrap has been the leader in free Bootstrap templates since 2013!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+  )
+}
+
+export default Item
